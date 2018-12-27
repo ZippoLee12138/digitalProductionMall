011f4eb983acfa9eb77d20d63bc784c513f13cfe
home <kulou322@126.com>
2018-12-28 00:26:35 +0800

    调整layout组件顶部视图样式

--- digitalProductionMall/src/App.vue
+++ digitalProductionMall/src/App.vue
@@ -7,11 +7,11 @@
 </template>
 
 <script>
-import layout from "./components/layout";
+import layout from './components/layout'
 
 export default {
   name: 'App',
-  components: {layout }
+  components: { layout }
 }
 </script>
 
@@ -22,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
--- digitalProductionMall/src/components/layout.vue
+++ digitalProductionMall/src/components/layout.vue
@@ -2,7 +2,10 @@
   <div>
     <div class="app-head">
       <div class="app-head-inner">
-        <img src="../assets/logo.png"  />
+        <router-link :to="{path: '/'}">
+            <img src="../assets/logo.png" class="app-head-img" />
+        </router-link>
+
         <div class="head-nav">
           <ul>
             <li>登录</li>
@@ -21,11 +24,69 @@ export default {
     return {
 
     }
-  }  
+  }
 }
 </script>
 
 <style>
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+body {
+  background: #f0f2f5;
+  font-family: "Helvetica Neue",Helvetica,Arial,"Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei UI","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
+  font-size: 14px;
+  color: #444;
+}
 
 .app-head {
     background: #363636;
@@ -35,12 +96,28 @@ export default {
     width: 100%;
 }
 
-.head-nav li{
+.head-nav {
+  /* display: inline-block; */
   float: right;
+}
+.head-nav ul {
+  overflow: hidden;
+}
+.head-nav li {
+  cursor: pointer;
+  float: left;
 }
 
 .app-head-inner {
-  display: inline-block;
+  /* display: inline-block; */
+  width: 1200px;
+  margin: 0 auto;
+}
+
+.app-head-img {
+    width: 50px;
+    margin-top: 20px;
+    float: left;
 }
 
 </style>
