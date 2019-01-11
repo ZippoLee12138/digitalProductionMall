a1c9ec1dec158c3a711c0ac5fe7f64b4220de933
lizebo <lizebo@koopoo.com.cn>
2019-01-11 21:07:28 +0800

    回退测试4444

--- digitalProductionMall/src/pages/index.vue
+++ digitalProductionMall/src/pages/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <p>首页内容 使用sourcetree测试回退</p>
+  <p>首页内容</p>
   <div flex="main:center cross:center" style="width:500px; height: 500px; background: #108423">
     <div style="background: #fff">看看我是不是居中的</div>
   </div>
   <div>再次测试回退</div>
   <div>回退测试333333</div>
+  <div>回退测试44444</div>
 </template>
 
 <script>
