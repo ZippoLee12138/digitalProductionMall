87ee7f463f25317642c9b010bbe972d9112169b4
lizebo <lizebo@koopoo.com.cn>
2019-01-11 20:54:34 +0800

    测试回退3

--- digitalProductionMall/src/pages/index.vue
+++ digitalProductionMall/src/pages/index.vue
@@ -3,6 +3,7 @@
   <div flex="main:center cross:center" style="width:500px; height: 500px; background: #108423">
     <div style="background: #fff">看看我是不是居中的</div>
   </div>
+  <div>再次测试回退</div>
 </template>
 
 <script>
