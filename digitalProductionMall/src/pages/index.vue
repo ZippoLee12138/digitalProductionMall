69c7c2bb0f8be5e1796e50ab71659d03cb2d6d90
home <kulou322@126.com>
2019-01-11 22:59:52 +0800

    删除flex.css

--- digitalProductionMall/src/pages/index.vue
+++ digitalProductionMall/src/pages/index.vue
@@ -1,8 +1,5 @@
 <template>
   <p>首页内容</p>
-  <div flex="main:center cross:center" style="width:500px; height: 500px; background: #108423">
-    <div style="background: #fff">看看我是不是居中的</div>
-  </div>
   <div>再次测试回退</div>
   <div>回退测试333333</div>
   <div>回退测试44444</div>
